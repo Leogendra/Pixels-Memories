6e4c9da0fa1e0f7bcdbd715b36dfb00af8330e11
Leogendra <le.magicien.annonyme@gmail.com>
2025-02-27 17:05:32 +0100

    update to v1.4.1

--- App/app/build.gradle.kts
+++ App/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.pixelsmemories"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.4"
+        versionCode = 5
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
